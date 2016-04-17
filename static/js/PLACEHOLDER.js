0914b993218cc2ee57dea24b72c04a173578eddc
cjang5 <chrismangggg@gmail.com>
2016-04-17 17:08:50 -0500

    Got login page lookin better

---
+++ static/js/PLACEHOLDER.js
@@ -0,0 +1,5 @@
+document.querySelector('span.logo').onclick = function() {
+  console.log(document.querySelector('input').offsetHeight);
+  console.log(document.querySelector('button').offsetHeight);
+
+}
