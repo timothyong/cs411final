b4ce70f58a9a75bd542aa7a85cbdd8eabc1d39b5
cjang5 <chrismangggg@gmail.com>
2016-04-18 16:19:08 -0500

    Got error handling looking kewl

--- static/js/PLACEHOLDER.js
+++ static/js/PLACEHOLDER.js
@@ -1,3 +1,25 @@
-document.querySelector('div.navbar').onclick = function() {
-  console.log(document.querySelector('div.navbar').offsetHeight);
+
+// document.querySelector('span.logo a').onclick = function() {
+//   console.log(document.querySelector('div.notification').offsetHeight);
+//   console.log("logo clicked!");
+//   var modal = document.querySelector('div.modal');
+//   modal.className += " moveUp";
+//   setTimeout(function() {
+//     modal.className = modal.className.replace(" moveUp", "");
+//   }, 2000);
+// }
+
+var displayNotif = function() {
+  alert("hello");
 }
+
+document.querySelector('span.logo a').onclick = function() {
+
+  var notif = document.querySelector('div.notification');
+  notif.className += " notif-active";
+  setTimeout(function() {
+    notif.className = notif.className.replace(" notif-active", "");
+  }, 3000);
+}
+
+
