5707e61e3d38f2bece877dba1174352278029d0d
cjang5 <chrismangggg@gmail.com>
2016-04-20 14:29:01 -0500

    Tooltips for registration errors

--- TODO.md
+++ TODO.md
@@ -1,14 +1,21 @@
 #Todo list
 
 ##Chris
-+ ~~Add new error message handling for register.html~~
-+ ~~Create actual error message and make it sexy~~
-+ ~~Make name required for registration?~~
 + Make fonts global with @font-face
-+ ~~Add error notifications for register.html~~
-+ ~~Fix button states for register.html~~
 + say "Hello, FIRSTNAME" instead of "Hello, USERNAME"
 + Links for login/register redirect @ bottom of modals need hover/active states
 + Maybe touch up notifications with ERROR: header or something
 + Make shit unselectable
++ Strip names for characters
+
+
+### Completed
++ ~~Add new error message handling for register.html~~
++ ~~Create actual error message and make it sexy~~
++ ~~Make name required for registration?~~
++ ~~Add error notifications for register.html~~
++ ~~Fix button states for register.html~~
 + ~~Autoselect first input forms on login/register for that good UX tho~~
++ ~~Rename tooltips and make other tooltips~~
++ ~~Also handle multiple tooltips at once, we don't want overlap!~~
++ ~~when user presses tab, stay on that input form~~
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 // Here we will just keep track of the state of the input forms
 // in order to make sure the user runs into as little errors as possible
 // during registration.
+
 // Get all the input forms
 var inputs = document.querySelectorAll('input');
 var fullname     = inputs[0];
@@ -8,26 +9,44 @@ var username     = inputs[1];
 var password     = inputs[2];
 var passwordConf = inputs[3];
 
+// Get all tooltips
+var fn_tooltip = document.querySelector('div.fullname-tooltip'),
+    u_tooltip  = document.querySelector('div.username-tooltip'),
+    p_tooltip  = document.querySelector('div.password-tooltip'),
+    pc_tooltip = document.querySelector('div.passwordConf-tooltip');
+
+// Reposition tooltips
+fn_tooltip.style.top  = fullname.offsetTop;
+fn_tooltip.style.left = fullname.offsetLeft + fullname.offsetWidth + 12;
+u_tooltip.style.top   = username.offsetTop;
+u_tooltip.style.left  = username.offsetLeft + username.offsetWidth + 12;
+p_tooltip.style.top   = password.offsetTop;
+p_tooltip.style.left  = password.offsetLeft + password.offsetWidth + 12;
+pc_tooltip.style.top  = passwordConf.offsetTop;
+pc_tooltip.style.left = passwordConf.offsetLeft + passwordConf.offsetWidth + 12;
+
 // Full name
 fullname.oninput = function() {
   if (fullname.value.length == 0 || fullname.value.length >= 3) {
     fullname.style.border = '1px solid rgb(125, 136, 255)';
-  } else {
-    fullname.style.border = '1px solid red';
+    fn_tooltip.style.opacity = 0;
   }
 }
 fullname.onblur = function() {
   if (fullname.value.length == 0 || fullname.value.length >= 3) {
     fullname.style.border = '1px solid rgba(125, 136, 255, 0.5)';
   } else {
-    fullname.style.border = '1px solid red';
+    fullname.style.border = '1px solid rgb(255, 112, 112)';
+
+    fn_tooltip.style.opacity = 1;
+    fullname.focus();
   }
 }
 fullname.onfocus = function() {
   if (fullname.value.length == 0 || fullname.value.length >= 3) {
     fullname.style.border = '1px solid rgb(125, 136, 255)';
   } else {
-    fullname.style.border = '1px solid red';
+    fullname.style.border = '1px solid rgb(255, 112, 112)';
   }
 }
 
@@ -35,55 +54,58 @@ fullname.onfocus = function() {
 username.oninput = function() {
   if (username.value.length == 0 || username.value.length >= 4) {
     username.style.border = '1px solid rgb(125, 136, 255)';
-  } else {
-    username.style.border = '1px solid red';
+    u_tooltip.style.opacity = 0;
   }
 }
 username.onblur = function() {
   if (username.value.length == 0 || username.value.length >= 4) {
     username.style.border = '1px solid rgba(125, 136, 255, 0.5)';
   } else {
-    username.style.border = '1px solid red';
+    username.style.border = '1px solid rgb(255, 112, 112)';
+
+    u_tooltip.style.opacity = 1;
+    username.focus();
   }
 }
 username.onfocus = function() {
   if (username.value.length == 0 || username.value.length >= 4) {
     username.style.border = '1px solid rgb(125, 136, 255)';
   } else {
-    username.style.border = '1px solid red';
+    username.style.border = '1px solid rgb(225, 112, 112)';
   }
 }
 
-
-// Passwords
+// Password
 password.oninput = function() {
   if (password.value.length == 0 || password.value.length >= 6) {
     password.style.border = '1px solid rgb(125, 136, 255)';
-  } else {
-    password.style.border = '1px solid red';
+    p_tooltip.style.opacity = 0;
   }
 }
 password.onblur = function() {
   if (password.value.length == 0 || password.value.length >= 6) {
     password.style.border = '1px solid rgba(125, 136, 255, 0.5)';
   } else {
-    password.style.border = '1px solid red';
+    password.style.border = '1px solid rgb(225, 112, 112)';
+
+    p_tooltip.style.opacity = 1;
+    password.focus();
   }
 }
 password.onfocus = function() {
   if (password.value.length == 0 || password.value.length >= 6) {
     password.style.border = '1px solid rgb(125, 136, 255)';
   } else {
-    password.style.border = '1px solid red';
+    password.style.border = '1px solid rgb(225, 112, 112)';
   }
 }
 
+// Password Confirmation
 passwordConf.oninput = function() {
   if (passwordConf.value.length == 0 || (passwordConf.value.length >= 6 &&
       passwordConf.value == password.value)) {
     passwordConf.style.border = '1px solid rgb(125, 136, 255)';
-  } else {
-    passwordConf.style.border = '1px solid red';
+    pc_tooltip.style.opacity = 0;
   }
 }
 passwordConf.onblur = function() {
@@ -91,7 +113,10 @@ passwordConf.onblur = function() {
       passwordConf.value == password.value)) {
     passwordConf.style.border = '1px solid rgba(125, 136, 255, 0.5)';
   } else {
-    passwordConf.style.border = '1px solid red';
+    passwordConf.style.border = '1px solid rgb(225, 112, 112)';
+
+    pc_tooltip.style.opacity = 1;
+    passwordConf.focus();
   }
 }
 passwordConf.onfocus = function() {
@@ -99,6 +124,6 @@ passwordConf.onfocus = function() {
       passwordConf.value == password.value)) {
     passwordConf.style.border = '1px solid rgb(125, 136, 255)';
   } else {
-    passwordConf.style.border = '1px solid red';
+    passwordConf.style.border = '1px solid rgb(225, 112, 112)';
   }
 }
